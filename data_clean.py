f13918a92629ed9c4f68676c2eb3e97314129ced
poon <poonhnhk@gmail.com>
2018-10-05 00:11:31 +0800

    add cleaning func and webbbrowser for profiling

--- data_clean.py
+++ data_clean.py
@@ -1,6 +1,43 @@
 import pandas as pd
 import numpy as np
 import pandas_profiling as prof
+import webbrowser
+import os
+import matplotlib.pyplot as plt
+import missingno as msno
 
 def get_RawProfile(df):
+    profile = prof.ProfileReport(df)
+    profile.to_file(outputfile="myoutputfile.html")
+    parent_path = os.getcwd()
+    path = parent_path + "/myoutputfile.html"
+    chrome_path = '/usr/bin/google-chrome %s'
+    webbrowser.get(chrome_path).open(path)
+    #msno.matrix(df)
     return prof.ProfileReport(df)
+
+def clean_missing(df,mode="drop",value=0, axis=0):
+    if mode == "drop":
+        return df.dropna(inplace=True, axis=axis)
+    elif mode == "fill":
+        return df.fillna(value)
+    else:
+        print("Wrong mode: only (drop / fill)")
+
+def clean_missing_pro(df, columns_to_fill, values_to_fill, axis=0):
+    typ = type(values_ro_fill)
+    if typ == "list":
+        if len(columns_to_fill) != len(values_to_fill):
+            raise ValueError('Size or columns and value to fill is not same.')
+        for i,col in enumerate(columns_to_fill):
+            df[col] = df[col].fillna(values_to_fill[i])
+    elif typ == "str" or typ == "int" or typ == "float" :
+        for col in columns_to_fill:
+            df[col] = df[col].fillna(values_to_fill)
+    else:
+        raise ValueError("Not a proper filling type.")
+
+    return df.dropna(inplace=True, axis=axis)
+        
+                
+    
--- main.py
+++ main.py
@@ -3,7 +3,13 @@ import numpy as np
 import os
 from data_clean import * 
 
-print(os.listdir("data/google_apps"))
-df = pd.read_csv("/home/yapws/Documents/Peter/Data-Science-Framework/data/google_apps/googleplaystore.csv")
+print(os.listdir("data/Telco"))
+#df = pd.read_csv("data/Telco/WA_Fn-UseC_-Telco-Customer-Churn.csv", parse_dates=True, encoding='UTF-8')
+df = pd.DataFrame(np.random.randn(5, 3), index=['a', 'c', 'e', 'f', 'h'],columns=['one', 'two', 'three'])
+df['four'] = 'bar'
+df['five'] = df['one'] > 0
+df = df.reindex(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
 plot = get_RawProfile(df)
+df = clean_missing(df, "fill", 999)
+print(df.head())
 
